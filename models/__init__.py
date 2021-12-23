322d2bda3a886692a0eaa219cc354cb92cc2e84d
Nicky-muindi <nicholasmuindi25@gmail.com>
2021-12-23 06:24:58 -0800

    updated-files

--- models/__init__.py
+++ models/__init__.py
@@ -1,29 +1,7 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""Instantiates a storage object.
-
--> If the environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
-   instantiates a database storage engine (DBStorage).
--> Otherwise, instantiates a file storage engine (FileStorage).
-"""
-from os import getenv
-
-
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.file_storage import FileStorage
-=======
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
-from os import getenv
 
-store_t = getenv("HBNB_TYPE_STORAGE")
 
-if store_t == "db":
-    storage = DBStorage()
-else:
->>>>>>> ebe09c5eae79e587efce1d62062a6740d55447a1
-    storage = FileStorage()
+storage = FileStorage()
 storage.reload()
--- models/amenity.py
+++ models/amenity.py
@@ -1,39 +1,7 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""Defines the Amenity class."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
-from sqlalchemy.orm import relationship
-
-
-class Amenity(BaseModel, Base):
-    """Represents an Amenity for a MySQL database.
-
-    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Amenities.
-        name (sqlalchemy String): The amenity name.
-        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
-=======
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
-class Amenity(BaseModel, Base):
-    """Represents Amenity"""
-    if models.storage_t == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
->>>>>>> ebe09c5eae79e587efce1d62062a6740d55447a1
+
+class Amenity(BaseModel):
+    name = ""
