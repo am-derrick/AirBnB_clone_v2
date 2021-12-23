f92e6acc825bd387fc4c582b62fbb807ea118fdd
Nicky-muindi <nicholasmuindi25@gmail.com>
2021-12-23 06:30:00 -0800

    updated-files

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -24,5 +24,4 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
->>>>>>> ebe09c5eae79e587efce1d62062a6740d55447a1
 FLUSH PRIVILEGES;
