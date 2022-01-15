d7e0907f896504bd07fd5429c100581707d4795f
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-01-15 09:36:43 +0300

    Errors fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,12 +18,12 @@ def do_deploy(archive_path):
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, end_t))
-        run('tar -xzf /tmp{} -C {}/'.format(file_name, path, end_t))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, end_t))
         run('rm /tmp/{}'.format(file_name))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, end_t))
         run('rm -rf {}{}/web_static'.format(path, end_t))
         run('rm -rf /data/web_static/current')
-        run('-ln -s {}/ /data/web_static/current'.format(path, end_t))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, end_t))
         return True
     except:
         return False
