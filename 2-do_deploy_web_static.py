5c68a5a13b6a3732fefca2320b2725ba0c0c1e2a
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-01-15 09:31:58 +0300

    Typos fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ Fabric script that distributes an archive to the web server
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = [34.74.59.91, 34.294.179.254]
+env.hosts = ['34.74.59.91', '34.294.179.254']
 
 
 def do_deploy(archive_path):
@@ -22,7 +22,7 @@ def do_deploy(archive_path):
         run('rm /tmp/{}'.format(file_name))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, end_t))
         run('rm -rf {}{}/web_static'.format(path, end_t))
-        run('rm -rf /data/web_Static/current')
+        run('rm -rf /data/web_static/current')
         run('-ln -s {}/ /data/web_static/current'.format(path, end_t))
         return True
     except:
