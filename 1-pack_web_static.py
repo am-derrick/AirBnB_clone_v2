2454833990b3b312e197b14a4411f7d2223ba6f1
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-01-15 09:28:27 +0300

    Function documented

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,7 +13,7 @@ def do_pack():
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
-            local("mkdir versions"):
+            local("mkdir versions")
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
