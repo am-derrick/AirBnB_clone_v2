b8db6f9e31aecc941570ce12edb9df7026e9de46
Derrick Ampire <ampire90@gamil.com>
2022-01-29 09:42:01 +0000

    First tasks

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """displays Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def showc(text):
+    """displays "C" followed by value of text"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def showpython(text='is cool'):
+    """displays "Python" followed by value of text"""
+    return 'Python ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """displays Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def showc(text):
+    """displays "C" followed by value of text"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/pyton/<text>', strict_slashes=False)
+def showpyhton(text='is cool'):
+    """displays "Python" followed by value of text"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def shownumber(n):
+    """displays "n is a number" only if n is an integer"""
+    return "{:d} is a number".format(n)
+
+
+@app.route('/number_template/<n>', strict_slashes=False)
+def numbertemplates(n):
+    """displays a HTML pahe only if n is an integer"""
+    return render_templates('5-number.html', n=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
